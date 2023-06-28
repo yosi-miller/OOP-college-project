d52c4bc96a2de3453d71cd0893878976505a929b
asherf <319adf@gmail.com>
2023-06-28 19:10:17 +0300

    יצירת קלאסים: תלמיד מורה מתרגל/מדריך מקצוע

--- profession_class.py
+++ profession_class.py
@@ -5,5 +5,6 @@ class Profession:
         self.hours = hours
         self.score = score
         # self.project = 0
+
     def switch_score(self, score):  # נתינת או שינוי ציון
         self.score = score
